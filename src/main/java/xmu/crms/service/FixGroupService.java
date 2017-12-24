bf8442e3bf5b07d8e72408c252d0e93523aa1f6b
Encyclomen <34083012+Encyclomen@users.noreply.github.com>
2017-12-24 10:26:23 +0800

    对insertStudentIntoGroup添加classId参数

--- src/main/java/xmu/crms/service/FixGroupService.java
+++ src/main/java/xmu/crms/service/FixGroupService.java
@@ -145,16 +145,18 @@ public interface FixGroupService {
      *
      * @param userId  学生的id
      * @param groupId 要加入小组的id
+     * @param classId 要加入小组所属于的班级的id
      * @return BigInteger 若创建成功返回该条记录的id，失败则返回-1
      * @throws IllegalArgumentException  信息不合法，id格式错误
      * @throws FixGroupNotFoundException 未找到小组
+     * @throws ClassesNotFoundException 未找到班级
      * @throws UserNotFoundException     不存在该学生
      * @throws InvalidOperationException 待添加学生已经在小组里了
      * @author YeHongjie
      */
 
-    public BigInteger insertStudentIntoGroup(BigInteger userId, BigInteger groupId) throws
-            IllegalArgumentException, FixGroupNotFoundException, UserNotFoundException,
+    public BigInteger insertStudentIntoGroup(BigInteger userId, BigInteger classId, BigInteger groupId) throws
+            IllegalArgumentException, FixGroupNotFoundException, UserNotFoundException,ClassesNotFoundException,
             InvalidOperationException;
 
     /**
