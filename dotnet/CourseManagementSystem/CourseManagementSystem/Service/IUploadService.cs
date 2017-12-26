c240ab5c7f316e69178117b01340bce0c98a4ea3
AiqiLiu <1965378683@qq.com>
2017-12-26 23:51:39 +0800

    Revert "DotNet-add UploadService""
    This reverts commit 75a74c0d4e31306b57a54cc1fd0a944572a33dd4.

--- dotnet/CourseManagementSystem/CourseManagementSystem/Service/IUploadService.cs
+++ dotnet/CourseManagementSystem/CourseManagementSystem/Service/IUploadService.cs
@@ -8,5 +8,28 @@ namespace Xmu.Crms.Shared.Service
     interface IUploadService
     {
 
+        ///<summary>
+        ///上传选课名单
+        ///老师上传本班级的学生名单
+        ///</summary>
+        ///<param name="classId">班级Id</param>
+        ///<param name="pathName">文件路径</param>
+        void UploadRoster(long classId, String pathName);
+
+        ///<summary>上传小组报告
+        ///上传讨论课的报告
+        ///</summary>
+        ///<param name="seminaId">讨论课Id</param>
+        ///<param name="pathName">文件路径</param>
+        void UploadReport(long seminaId, String pathName);
+
+        ///<summary>
+        ///上传用户头像名单
+        ///上传用户头像
+        ///</summary>
+        ///<param name="userId">用户ID</param>
+        ///<param name="pathName">文件路径</param>
+        void UploadAvater(long userId, String pathName);
+
     }
 }
