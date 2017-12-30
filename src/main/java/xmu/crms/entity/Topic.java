17c2af02a80378586555ca869c32abdd57eb4aaa
moonlight <1241621063@qq.com>
2017-12-30 18:01:33 +0800

    Update entity

--- src/main/java/xmu/crms/entity/Topic.java
+++ src/main/java/xmu/crms/entity/Topic.java
@@ -5,44 +5,64 @@ import java.math.BigInteger;
 public class Topic {
 	private BigInteger id;
 	private String name;
+	private String serial;
 	private String description;
 	private Integer groupNumberLimit;
 	private Integer groupStudentLimit;
 	private Seminar seminar;
-	
+
 	public BigInteger getId() {
 		return id;
 	}
+
 	public void setId(BigInteger id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getSerial() {
+		return serial;
+	}
+
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Integer getGroupNumberLimit() {
 		return groupNumberLimit;
 	}
+
 	public void setGroupNumberLimit(Integer groupNumberLimit) {
 		this.groupNumberLimit = groupNumberLimit;
 	}
+
 	public Integer getGroupStudentLimit() {
 		return groupStudentLimit;
 	}
+
 	public void setGroupStudentLimit(Integer groupStudentLimit) {
 		this.groupStudentLimit = groupStudentLimit;
 	}
+
 	public Seminar getSeminar() {
 		return seminar;
 	}
+
 	public void setSeminar(Seminar seminar) {
 		this.seminar = seminar;
 	}
@@ -52,6 +72,7 @@ public class Topic {
 		return "Topic{" +
 				"id=" + id +
 				", name='" + name + '\'' +
+				", serial='" + serial + '\'' +
 				", description='" + description + '\'' +
 				", groupNumberLimit=" + groupNumberLimit +
 				", groupStudentLimit=" + groupStudentLimit +
